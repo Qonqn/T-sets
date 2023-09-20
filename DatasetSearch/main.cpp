15985c8d8491151b8547e72a267f0c95dd580746
Qonqn <1169670230@qq.com>
2023-09-20 14:31:48 +0800

    complete top-k simtraj

--- DatasetSearch/main.cpp
+++ DatasetSearch/main.cpp
@@ -22,9 +22,36 @@ struct TrajectorySimilarity {
     // 重载小于运算符，用于比较轨迹相似度
     bool operator<(const TrajectorySimilarity& other) const {
         // 按照相似度的降序进行排序
-        return similarity < other.similarity;
+        return similarity > other.similarity;
     }
 };
+
+struct SpaceSimilarity {
+    int index;
+    double minsimilarity;
+
+    // 重载小于运算符，用于比较轨迹相似度
+    bool operator<(const SpaceSimilarity& other) const {
+        // 按照相似度的降序进行排序
+        if(minsimilarity==other.minsimilarity){
+            return index>other.index;
+        }
+        return minsimilarity > other.minsimilarity;
+    }
+};
+
+struct smallerSpaceSimilarity {
+    int index;
+    int positioncode;
+    double minsimilarity;
+
+    // 重载小于运算符，用于比较轨迹相似度
+    bool operator<(const smallerSpaceSimilarity& other) const {
+        // 按照相似度的降序进行排序
+        return minsimilarity > other.minsimilarity;
+    }
+};
+
 void Split(const std::string& src, const std::string& separator, std::vector<std::string>& dest) //字符串分割到数组
 {
 	//参数1：要分割的字符串；参数2：作为分隔符的字符；参数3：存放分割后的字符串的vector向量
@@ -577,62 +604,305 @@ double lowerbound(vector<pair<double,double>> query_traj,int space_key){
 
 }
 
-priority_queue<TrajectorySimilarity> Top_k_similar_trajectory_search(shared_ptr<ZRedisConnection> con,string traj_str,int k){
-    priority_queue<TrajectorySimilarity> trajectoryQueue;
-    double threshold=(numeric_limits<double>::max)();
-    vector<pair<double,double>> query_traj=split_trajectory_points(traj_str);
-    int initial_index=space_index(1,query_traj,120.487,30.999,121.809,31.477);
-    string initial_indexStr=to_string(initial_index);
-    int index=0;
-    int positioncode=0;
-    if(initial_indexStr[0]!='2'){
-         positioncode=initial_index%10;
-         if(!positioncode) positioncode=10;
-         index=initial_index/10;
+vector<double> index_to_space(int index){
+    vector<double> res;
+    double x_min=120.487;
+    double y_min=30.999;
+    double x_max=121.809;
+    double y_max=31.477;
+    //here is the root space
+    string index_str=to_string(index);
+    for(int i=1;i<index_str.size();i++){
+        if(index_str[i]=='0'){
+            y_max=(y_min+y_max)/2;
+            x_max=(x_min+x_max)/2;
+        }
+        else if(index_str[i]=='1'){
+            y_max=(y_min+y_max)/2;
+            x_min=(x_min+x_max)/2;
+        }
+        else if(index_str[i]=='2'){
+            y_min=(y_min+y_max)/2;
+            x_max=(x_min+x_max)/2;
+        }
+        else{
+            y_min=(y_min+y_max)/2;
+            x_min=(x_min+x_max)/2;
+        }
     }
-    else{
-        positioncode=11;
-        index=changeFirstDigit_1(initial_index);
-    }
-    int current_index=index;
-    while(true){
-        string cmd="keys "+to_string(current_index);
-        RedisResult res1;
-        con->ExecCmd(cmd,res1);
-        for(int i=0;i<res1.vecdata.size();i++){
-            cmd="hget "+res1.vecdata[i]+" track";
-            RedisResult res2;
-            con->ExecCmd(cmd,res2);
-            vector<pair<double,double>> traj=split_trajectory_points(res2.strdata);
-            int distance=computeDistance(query_traj,traj);
-            if(distance<threshold){
-                TrajectorySimilarity new_element;
-                new_element.trajectoryId=res1.vecdata[i];
-                new_element.similarity=distance;
-                trajectoryQueue.push(new_element);
-                if(trajectoryQueue.size()>k){
-                    trajectoryQueue.pop();
-                    
-                }
-            }      
-        } 
-    }
+    res.push_back(x_min);
+    res.push_back(y_min);
+    res.push_back(x_max);
+    res.push_back(y_max);
+    return res;
+}
 
+double minDistTS(int index,vector<pair<double,double>> query_traj){
+    vector<double> space=index_to_space(index);
+    double x_min=space[0];
+    double y_min=space[1];
+    double x_max=space[2];
+    double y_max=space[3];
+    double maxDistance = 0.0;
+    
+    for (const std::pair<double, double>& point : query_traj) {
+        double x = point.first;
+        double y = point.second;
+        
+        // 判断点是否在矩形范围内
+        if (x >= x_min && x <= x_max && y >= y_min && y <= y_max) {
+            // 点在矩形内，距离为0
+            continue;
+        }
+        
+        // 计算点到矩形四个角的距离
+        double distances[4];
+        distances[0] = distance(point,make_pair(x_min,y_min)); 
+        distances[1] = distance(point,make_pair(x_max,y_min));
+        distances[2] = distance(point,make_pair(x_min,y_max));
+        distances[3] = distance(point,make_pair(x_max,y_max));
+        
+        // 找到最大距离
+        double pointMaxDistance = distances[0];
+        for (int i = 1; i < 4; ++i) {
+            if (distances[i] > pointMaxDistance) {
+                pointMaxDistance = distances[i];
+            }
+        }
+        
+        // 更新全局最大距离
+        if (pointMaxDistance > maxDistance) {
+            maxDistance = pointMaxDistance;
+        }
+    }
+    
+    return maxDistance;
+}
+
+double minDistTsS(int index,int positioncode,vector<pair<double,double>> query_traj){
+    double res=0;
+    double minDist[4];
+    for(int i=0;i<4;i++){
+        double smaller_index=index*10+i;
+        minDist[i]=minDistTS(smaller_index,query_traj);
+    }
+    if(!minDistTS(index,query_traj)){
+        vector<double> space=index_to_space(index);
+        int initial_index=space_index(index,query_traj,space[0],space[1],space[2],space[3]);
+        string initial_indexStr=to_string(initial_index);
+        int position_code=0;
+        if(initial_indexStr[0]!='2'){
+           position_code=initial_index%10;
+           if(!position_code) position_code=10;
+        }
+        else{
+           position_code=11;
+        }
+        if(position_code==positioncode)
+           return 0;
+    }
+    switch (positioncode)
+    {
+    case 1/* constant-expression */:
+        /* code */
+        res=max(minDist[0],minDist[1]);
+        break;
+    case 2:
+        res=max(minDist[0],minDist[2]);
+        break;
+    case 3:
+        res=max(minDist[0],minDist[3]);
+        break;
+    case 4:
+        res=max(minDist[1],minDist[2]);
+        break;
+    case 5:
+        res=max(minDist[1],minDist[3]);
+        break;
+    case 6:
+        res=max(minDist[2],minDist[3]);
+        break;
+    case 7:
+        res=max(max(minDist[1],minDist[2]),minDist[3]);
+        break;
+    case 8:
+        res=max(max(minDist[0],minDist[2]),minDist[3]);
+        break;
+    case 9:
+        res=max(max(minDist[0],minDist[1]),minDist[3]);
+        break;
+    case 10:
+        res=max(max(minDist[0],minDist[1]),minDist[2]);
+        break;
+    case 11:
+        res=max(max(minDist[0],minDist[1]),max(minDist[2],minDist[3]));
+        break;
+    default:
+        break;
+    }
+    return res;
+}
+
+double DistTT(shared_ptr<ZRedisConnection> con,string tid,vector<pair<double,double>> query_traj){
+    //here tid is the complete index of traj
+    RedisResult res;
+    string cmd="hget "+tid+" track";
+    con->ExecCmd(cmd,res);
+    vector<pair<double,double>> traj=split_trajectory_points(res.strdata);
+    return computeDistance(traj,query_traj);    
+}
+
+vector<string> trajInSmallerSpace(shared_ptr<ZRedisConnection> con,int index,int positioncode){
+    vector<string> trajs;
+    RedisResult res;
+    string cmd="keys "+to_string(index)+"#*";
+    con->ExecCmd(cmd,res);
+    if(index==13212&&positioncode==1){
+        cout<<res.vecdata.size()<<endl;
+    }
+    for(int i=0;i<res.vecdata.size();i++){
+        cmd="hget "+res.vecdata[i]+" position_code";
+        RedisResult res2;
+        con->ExecCmd(cmd,res2);
+        if(index==13212&&positioncode==1){
+           cout<<res2.strdata<<endl;
+        }
+        
+        if(res2.strdata==to_string(positioncode)){
+            trajs.push_back(res.vecdata[i]);
+        }
+    }
+    return trajs;
+}
+
+double updateQueue(priority_queue<TrajectorySimilarity>& q,int k){
+   //todo:resize queue and return new threshold
+   double threshold=0;
+   if(q.size()<k)
+       return (numeric_limits<double>::max)();
+   else if(q.size()==k){
+       priority_queue<TrajectorySimilarity> tq_copy = q;
+       while (!tq_copy.empty()) {
+          TrajectorySimilarity top_element = tq_copy.top();
+          threshold=top_element.similarity;
+          tq_copy.pop();
+       }
+       return threshold;
+   }
+   else{
+       priority_queue<TrajectorySimilarity> tq_copy = q;
+       for(int i=0;i<k;i++){
+          TrajectorySimilarity top_element = q.top();
+          cout<<top_element.similarity<<endl;
+          threshold=top_element.similarity;
+          q.pop();
+       }
+       q.pop();
+       for(int i=0;i<k;i++){
+          TrajectorySimilarity top_element = tq_copy.top();
+          tq_copy.pop();
+          q.push(top_element);
+       }
+       return threshold;
+   }
+}
+
+priority_queue<TrajectorySimilarity> Top_k_similar_trajectory_search(shared_ptr<ZRedisConnection> con,string traj_str,int k){
+    vector<pair<double,double>> query_traj=split_trajectory_points(traj_str);
+    priority_queue<TrajectorySimilarity> trajectoryQueue;
+    priority_queue<SpaceSimilarity> spaceQueue;
+    SpaceSimilarity rootSpace;
+    rootSpace.index=1;
+    rootSpace.minsimilarity=minDistTS(1,query_traj);
+    spaceQueue.push(rootSpace);
+    priority_queue<smallerSpaceSimilarity> smallerSpaceQueue;
+    double threshold=(numeric_limits<double>::max)();
+    while(!spaceQueue.empty()){
+        cout<<"current threshold is "<<threshold<<endl;
+        SpaceSimilarity space=spaceQueue.top();
+        cout<<"current space's index is "<<space.index<<endl;
+        spaceQueue.pop();
+        if(!smallerSpaceQueue.empty()){
+            cout<<"smallerSpace minsim is "<< smallerSpaceQueue.top().minsimilarity<<endl;
+            cout<<"Space minsim is "<<space.minsimilarity<<endl;
+        }
+        if(smallerSpaceQueue.empty()||smallerSpaceQueue.top().minsimilarity>=space.minsimilarity){
+            if(space.minsimilarity>threshold)
+               return trajectoryQueue;
+            for(int i=1;i<=11;i++){
+                smallerSpaceSimilarity smallerSpace;
+                smallerSpace.index=space.index;
+                smallerSpace.positioncode=i;
+                smallerSpace.minsimilarity=minDistTsS(space.index,i,query_traj);
+                cout<<smallerSpace.minsimilarity<<endl;
+                smallerSpaceQueue.push(smallerSpace);
+            }
+            for(int i=0;i<=3;i++){
+                SpaceSimilarity childSpace;
+                childSpace.index=space.index*10+i;
+                RedisResult res;
+                string cmd="keys "+to_string(childSpace.index)+"#*";
+                con->ExecCmd(cmd,res);
+                int traj_count=res.vecdata.size();
+                if(!traj_count) continue;
+                childSpace.minsimilarity=minDistTS(childSpace.index,query_traj);
+                cout<<childSpace.minsimilarity<<endl;
+                spaceQueue.push(childSpace);
+            }
+        }
+        else{
+            while(!smallerSpaceQueue.empty() && smallerSpaceQueue.top().minsimilarity<=space.minsimilarity){
+                smallerSpaceSimilarity smallerSpace=smallerSpaceQueue.top();
+                smallerSpaceQueue.pop();
+                cout<<smallerSpace.index<<" "<<smallerSpace.positioncode<<endl;
+                if(smallerSpace.minsimilarity>threshold)
+                   return trajectoryQueue;
+                vector<string> candidateTid=trajInSmallerSpace(con,smallerSpace.index,smallerSpace.positioncode);
+                cout<<"candidate traj's size is "<<candidateTid.size()<<endl;
+                for(int i=0;i<candidateTid.size();i++){
+                    double dist=DistTT(con,candidateTid[i],query_traj);
+                    if(dist<threshold){
+                        TrajectorySimilarity traj;
+                        traj.trajectoryId=candidateTid[i];
+                        
+                        traj.similarity=dist;
+                        trajectoryQueue.push(traj);
+                        threshold=updateQueue(trajectoryQueue,k);
+                        if(traj.trajectoryId=="13212#78387"){
+                            cout<<"arrive here "<<dist<<endl;
+                            cout<<"threshold is "<<threshold<<endl;
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
 
 int main(){
     ZRedisConnectionPool::init("127.0.0.1",6379,"123456",1);
-	shared_ptr<ZRedisConnection> con = ZRedisConnectionPool::Get();
+    shared_ptr<ZRedisConnection> con=ZRedisConnectionPool::Get();
     initial_table();
     set<string> metadata=search_metadata(con);
     set<string> test;
     test.insert("13212#78387");
     //search_by_space(con,121.348,121.357,31.388,31.389);
-    set<string> tb_simtraj=Tbsimilar_trajectory_search(con,"121.348,31.389;121.347,31.392;121.348,31.389;121.349,31.390;121.350,31.390;121.351,31.390;121.351,31.391;121.352,31.391;121.353,31.391;121.353,31.394;121.354,31.391;121.355,31.391;121.356,31.389;121.356,31.390;121.356,31.391;121.357,31.388;121.357,31.389;121.357,31.388",0.05);
+    string query_traj="121.348,31.389;121.347,31.392;121.348,31.389;121.349,31.390;121.350,31.390;121.351,31.390;121.351,31.391;121.352,31.391;121.353,31.391;121.353,31.394;121.354,31.391;121.355,31.391;121.356,31.389;121.356,31.390;121.356,31.391;121.357,31.388;121.357,31.389;121.357,31.388";
+    /*
+    set<string> tb_simtraj=Tbsimilar_trajectory_search(con,query_traj,0.03);
     cout<<"tb_simtraj'size is "<<tb_simtraj.size()<<endl;
     for(set<string>::iterator i=tb_simtraj.begin();i!=tb_simtraj.end();i++){
         cout<<*i<<endl;
     }
+    */
+    priority_queue<TrajectorySimilarity> top_k_simTraj=Top_k_similar_trajectory_search(con,query_traj,50);
+    
+    while(!top_k_simTraj.empty()){
+        TrajectorySimilarity res=top_k_simTraj.top();
+        cout<<res.trajectoryId<<" "<<res.similarity<<endl;
+        top_k_simTraj.pop();
+    }
+    
     //generate_dataset(con,test);
     /*for(set<string>::iterator it=metadata.begin();it!=metadata.end();it++){
         cout<<*it<<endl;
